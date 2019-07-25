3542685454434df3726838a514880ef58491dabb
Yurii Bondarenko <ybondarenko.job@gmail.com>
2019-07-25 10:50:49 +0300

    Adds new features to calendar

--- src/app/components/calendar/calendar.ts
+++ src/app/components/calendar/calendar.ts
@@ -1210,9 +1210,8 @@ export class Calendar implements OnInit,OnDestroy,ControlValueAccessor {
     }
 
     isDatesEquals(date1, date2) {
-        if (date1 && date2) {
+        if (date1 && date2)
             return date1.getDate() === date2.getDate() && date1.getMonth() === date2.getMonth() && date1.getFullYear() === date2.getFullYear();
-        }
         else
             return false;
     }
